f385d5a64a8c24c3dcc1f92dcd780956ed3fab28
Tameem-Elmasry <tameeemelmasry@gmail.com>
2025-07-16 14:14:23 +0300

    frontend ready for deployment

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const BASE_URL =
     import.meta.env.MODE === "development"
-        ? "http://localhost:5001/api"
-        : "/api";
+        ? `${import.meta.env.VITE_DEV_URL}/api`
+        : `${import.meta.env.VITE_API_URL}/api`;
 const api = axios.create({
     baseURL: BASE_URL,
     withCredentials: true,
